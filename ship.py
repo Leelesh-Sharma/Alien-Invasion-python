844cf7bee8e894f8bec31b3ab8fa1f3f935b4f1a
Leelesh Sharma <60009872+Leelesh-Sharma@users.noreply.github.com>
2021-10-13 17:01:27 +0530

    both side movment

--- ship.py
+++ ship.py
@@ -9,6 +9,7 @@ class Ship():
         self.screen_rect = screen.get_rect()
 
         self.moving_right = False
+        self.moving_left = False
 
         self.rect.centerx = self.screen_rect.centerx
         self.rect.bottom = self.screen_rect.bottom
@@ -16,6 +17,8 @@ class Ship():
     def update(self):
         if self.moving_right:
             self.rect.centerx += 1
+        if self.moving_left:
+            self.rect.centerx -= 1
 
     def blitme(self):
         self.screen.blit(self.image, self.rect)
