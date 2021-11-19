c4157513bb9da86f29b085c2cb1562035e19b4b4
Leelesh Sharma <60009872+Leelesh-Sharma@users.noreply.github.com>
2021-11-19 09:56:32 +0530

    alien fleet created

--- settings.py
+++ settings.py
@@ -3,7 +3,7 @@ class Settings():
     def __init__(self):
         self.screen_width = 1200
         self.screen_height = 800
-        self.bg_color = (0, 255, 0)
+        self.bg_color = (255, 255, 255)
         self.ship_speed_factor = 1.5
 
         # Bullet Settings
